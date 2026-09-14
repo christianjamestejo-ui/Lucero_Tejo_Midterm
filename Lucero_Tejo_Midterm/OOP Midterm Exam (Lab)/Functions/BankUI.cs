259f65585223880c4d7d667dd99e90718c7171a1
christianjamestejo-ui <christianjames.tejo@dbtc-cebu.edu.ph>
2026-09-14 15:07:00 +0800

    Midterm Exam

---
+++ Lucero_Tejo_Midterm/OOP Midterm Exam (Lab)/Functions/BankUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Midterm_Exam__Lab_.Functions
+{
+    public static class BankUI
+    {
+        public static void ShowMenu() => Console.Write("1. View Summary | 2. Deposit | 3. Withdraw | 4. Exit"
+            + "\nChoose (1-4): ");
+
+            
+        public static double ReadPositiveAmount(string prompt)
+        {
+            //To do 5
+            while (true)
+            {
+                double.TryParse(prompt = Console.ReadLine(), out double validValue);
+                if (validValue > 0)
+                {
+                    return validValue;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Lucero_Tejo_Midterm/OOP Midterm Exam (Lab)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Principal;
+using OOP_Midterm_Exam__Lab_.Holder;
+using OOP_Midterm_Exam__Lab_.Functions;
+using System.Linq.Expressions;
+
+Console.Write("Enter Account Number: ");
+string num = Console.ReadLine();
+Console.Write("Enter Account Holder: ");
+string name = Console.ReadLine();
+double initBal = BankUI.ReadPositiveAmount(@"Enter Opening Balance: PHP ");
+
+// To do 6
+BankAccount account = new BankAccount(num, name, initBal);
+
+bool run = true;
+while (run)
+{
+    BankUI.ShowMenu();
+    int.TryParse(Console.ReadLine(), out int ch);
+
+    switch (ch)
+    {
+        case 1:
+            Console.WriteLine(account.GetSummary());
+            break;
+        case 2:
+            double dAmt =
+            BankUI.ReadPositiveAmount("Deposit Amount: PHP ");
+            if (account.Deposit(dAmt))
+            {
+                Console.WriteLine($"Deposited PHP {dAmt:N2}. Balance: PHP {account.Balance:N2}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit failed! Invalid amount.");
+            }
+            break;
+        case 3:
+            double wAmt = BankUI.ReadPositiveAmount("Withdraw Amount: PHP ");
+            if (account.Withdraw(wAmt))
+            {
+                Console.WriteLine($"Withdrew PHP {wAmt:N2}. Balance : PHP {account.Balance:N2}");
+            }
+            else
+            {
+                Console.WriteLine("Withdrawal failed! Insufficient funds.");
+            }
+            break;
+        case 4:
+            // To do 7
+            Console.WriteLine("Goodbye!");
+            run = false;
+            break;
+        default:
+            Console.WriteLine("Invalid selection! Try again.");
+            break;
+    }
+}
